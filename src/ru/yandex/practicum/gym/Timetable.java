f43273bc3a4635f2a0ab4797b6e97fc2667121d4
Яшар Исмаилов <Yashar>
2026-05-23 14:33:49 +0300

    Написать методы

--- src/ru/yandex/practicum/gym/Timetable.java
+++ src/ru/yandex/practicum/gym/Timetable.java
@@ -1,20 +1,68 @@
 package ru.yandex.practicum.gym;
 
+import javax.sql.rowset.spi.TransactionalWriter;
 import java.util.*;
 
 public class Timetable {
 
-    private /* как это хранить??? */ timetable;
+    private HashMap<DayOfWeek, TreeMap<TimeOfDay, List<TrainingSession>>> timetable = new HashMap<>();
 
     public void addNewTrainingSession(TrainingSession trainingSession) {
         //сохраняем занятие в расписании
+        if (trainingSession == null || trainingSession.getDayOfWeek() == null || trainingSession.getTimeOfDay() == null) {
+
+            System.out.println("Добавить запись не удалось");
+            return;
+        }
+        DayOfWeek day = trainingSession.getDayOfWeek();
+        TimeOfDay time = trainingSession.getTimeOfDay();
+
+        if (!timetable.containsKey(day)) {
+            timetable.put(day, new TreeMap<>());
+        }
+
+        TreeMap<TimeOfDay, List<TrainingSession>> todayGym = timetable.get(day);
+        if(!todayGym.containsKey(time)){
+            todayGym.put(time, new ArrayList<>());
+        }
+        List<TrainingSession> sessions = todayGym.get(time);
+        sessions.add(trainingSession);
+
+        System.out.println("Занятие успешно добавлено!");
     }
 
-    public /* непонятно, что возвращать */ getTrainingSessionsForDay(DayOfWeek dayOfWeek) {
+    public TreeMap<TimeOfDay, List<TrainingSession>> getTrainingSessionsForDay(DayOfWeek dayOfWeek) {
         //как реализовать, тоже непонятно, но сложность должна быть О(1)
+        if (timetable.containsKey(dayOfWeek)){
+            return timetable.get(dayOfWeek);
+        }
+        return new TreeMap<>();
     }
 
-    public /* непонятно, что возвращать */ getTrainingSessionsForDayAndTime(DayOfWeek dayOfWeek, TimeOfDay timeOfDay) {
+    public List<TrainingSession> getTrainingSessionsForDayAndTime(DayOfWeek dayOfWeek, TimeOfDay timeOfDay) {
         //как реализовать, тоже непонятно, но сложность должна быть О(1)
+        if (!timetable.containsKey(dayOfWeek)){
+            return new ArrayList<>();
+        }
+        TreeMap<TimeOfDay, List<TrainingSession>> todayGym = timetable.get(dayOfWeek);
+        if (!todayGym.containsKey(timeOfDay)){
+            return new ArrayList<>();
+        }
+        return todayGym.get(timeOfDay);
+    }
+    public LinkedHashMap<Coach,Integer> getCountByCoaches(){
+        Map<Coach,Integer> couchCount = new HashMap<>();
+        for (TreeMap<TimeOfDay, List<TrainingSession>> todayGym : timetable.values()){
+            for (List<TrainingSession> sessions : todayGym.values()){
+                for (TrainingSession trainingSession : sessions){
+                    Coach coach = trainingSession.getCoach();
+                    if (couchCount.containsKey(coach)){
+                        couchCount.put(coach,couchCount.get(coach)+1);
+                    }
+                    else couchCount.put(coach,1);
+                }
+            }
+        }
+
     }
 }
